af0f8fbca979416e37aa79a683384aca1cb427b6
Ivan <ivanprikhodko2004@gmail.com>
2022-12-24 12:06:19 +0300

    four step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine;
+import picocli.CommandLine.*;
+
+import java.io.File;
+
+@Command(
+        name = "gendiff",
+        version = "gendiff 2.28",
+        mixinStandardHelpOptions = true
+)
+public class App implements Runnable {
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+    }
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    String format;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new CommandLine(new App()).execute(args);
     }
 }
